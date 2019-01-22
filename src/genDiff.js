14b56080c3ad811b337aeaccd6e40ce1f67e0ceb
MSolovyanova <solovyanovamaria@yandex.ru>
2019-01-22 18:10:29 +0300

    changed genDiff

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,7 +1,5 @@
 import fs from 'fs';
-import { has } from 'lodash/fp';
-import { extname } from 'path';
-import parse from './parsers';
+import { has, union } from 'lodash';
 
 const getFileData = path => fs.readFileSync(path, 'utf-8');
 
@@ -9,38 +7,32 @@ const genDiff = (pathToFirstFile, pathToSecondFile) => {
   const firstFile = getFileData(pathToFirstFile);
   const secondFile = getFileData(pathToSecondFile);
 
-  const typeOfFirstFile = extname(pathToFirstFile);
-  const typeOfSecondFile = extname(pathToSecondFile);
-
-  console.log(`first file is ${typeOfFirstFile} and second file is ${typeOfSecondFile}`);
-
-  const firstFormatted = parse(typeOfFirstFile)(firstFile);
-  console.log('ff ', firstFormatted);
-
-  const secondFormatted = parse(typeOfSecondFile)(secondFile);
-  console.log('sf ', secondFormatted);
+  const firstFormatted = JSON.parse(firstFile);
+  const secondFormatted = JSON.parse(secondFile);
 
   const firstKeys = Object.keys(firstFormatted);
-  const compareFirstResult = firstKeys.reduce((acc, currentKey) => {
-    if (!has(secondFormatted, currentKey)) {
-      acc[`- ${currentKey}`] = firstFormatted[currentKey];
-    } else if (firstFormatted[currentKey] === secondFormatted[currentKey]) {
-      acc[currentKey] = firstFormatted[currentKey];
-    } else {
-      acc[`- ${currentKey}`] = firstFormatted[currentKey];
-      acc[`+ ${currentKey}`] = secondFormatted[currentKey];
-    }
-    return acc;
-  }, {});
-
   const secondKeys = Object.keys(secondFormatted);
-  const result = secondKeys.reduce((acc, currentKey) => {
-    if (!has(firstFormatted, currentKey)) {
-      acc[`+ ${currentKey}`] = secondFormatted[currentKey];
-    }
-    return acc;
-  }, compareFirstResult);
+  const unatedKeys = union(firstKeys, secondKeys);
 
+  const result = {};
+
+  unatedKeys.map((key) => {
+    if (has(firstFormatted, key) && has(secondFormatted, key)) {
+      if (firstFormatted[key] === secondFormatted[key]) {
+        result[key] = firstFormatted[key];
+      } else {
+        result[`- ${key}`] = firstFormatted[key];
+        result[`+ ${key}`] = secondFormatted[key];
+      }
+    } else {
+      if (has(firstFormatted, key) && !has(secondFormatted, key)) {
+        result[`- ${key}`] = firstFormatted[key];
+      }
+      if (!has(firstFormatted, key) && has(secondFormatted, key)) {
+        result[`+ ${key}`] = secondFormatted[key];
+      }
+    }
+  });
   return JSON.stringify(result);
 };
 

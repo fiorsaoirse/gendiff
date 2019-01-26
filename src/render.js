2afe8084c7892405bea9ec3dedf25ddb82b115d1
MSolovyanova <solovyanovamaria@yandex.ru>
2019-01-26 13:45:07 +0300

    fixed step 5

--- src/render.js
+++ src/render.js
@@ -1,71 +1,61 @@
 import { flatten } from 'lodash';
 
-const stringify = (value, depth, spaces) => {
+const stringify = (value, depth, spaces, step) => {
   if (value instanceof Object) {
     const valueKeys = Object.keys(value);
-    //console.log('value ', JSON.stringify(value));    
     const mapped = valueKeys.map(key => `  ${key}: ${value[key]}`).join('');
-    //console.log('mapped ', mapped);
-    //console.log('spaces [', spaces, ']');
-    //console.log(`{\n${spaces.repeat(depth)}${mapped}\n${spaces.repeat(depth)}}`);
-    return `{\n${spaces.repeat(depth + 1)}${mapped}\n${spaces.repeat(depth + 1)}}`;
+    const indent = depth * step - 2;
+    const indentNext = (depth + 1) * step - 2;
+    return `{\n${spaces.repeat(indentNext)}${mapped}\n  ${spaces.repeat(indent)}}`;
   }
   return value;
 };
 
-const typesOfNode = [
-  {
-    check: typeOfNode => typeOfNode === 'nested',
-    makeString: (record, currentDepth, fn, spaces) => {
-      const { key } = record;
-      const value = flatten(record.children.map(currentChild => fn(currentChild, currentDepth + 1)))
-                    .join('\n');
-      return `${spaces.repeat(currentDepth)}  ${key}: {\n${value}\n${spaces.repeat(currentDepth)}}`
-    }
-  },
-  {
-    check: typeOfNode => typeOfNode === 'added',
-    makeString: (record, currentDepth, fn, spaces) => {
-      const { key } = record;
-      const value = stringify(record.value, currentDepth, spaces);
-      return `${spaces.repeat(currentDepth)}+ ${key}: ${value}`;
-    }
-  },
-  {
-    check: typeOfNode => typeOfNode === 'deleted',
-    makeString: (record, currentDepth, fn, spaces) => {
-      const { key } = record;
-      const value = stringify(record.value, currentDepth, spaces);
-      return `${spaces.repeat(currentDepth)}- ${key}: ${value}`;
-    }
-  },
-  {
-    check: typeOfNode => typeOfNode === 'equal',
-    makeString: (record, currentDepth, fn, spaces) => {
-      const { key } = record;
-      const value = stringify(record.value, currentDepth, spaces);
-      return `${spaces.repeat(currentDepth)}  ${key}: ${value}`;
-    }
-  },
-  {
-    check: typeOfNode => typeOfNode === 'changed',
-    makeString: (record, currentDepth, fn, spaces) => {
-      const { key } = record;
-      const valueOld = stringify(record.valueOld, currentDepth, spaces);
-      const valueNew = stringify(record.valueNew, currentDepth, spaces);
-      return `${spaces.repeat(currentDepth)}- ${key}: ${valueOld}\n${spaces.repeat(currentDepth)}+ ${key}: ${valueNew}`;
-    }
-  }
-];
+const typesOfNode = {
 
-const checkNode = arg => typesOfNode.find(({ check }) => check(arg));
+  nested: (record, currentDepth, fn, spaces, step) => {
+    const { key } = record;
+    const value = flatten(record.children.map(currentChild => fn(currentChild, currentDepth + 1)))
+      .join('\n');
+    const indent = currentDepth * step - 2;
+    return `${spaces.repeat(indent)}  ${key}: {\n${value}\n  ${spaces.repeat(indent)}}`;
+  },
+  added: (record, currentDepth, fn, spaces, step) => {
+    const { key } = record;
+    const value = stringify(record.value, currentDepth, spaces, step);
+    const indent = currentDepth * step - 2;
+    return `${spaces.repeat(indent)}+ ${key}: ${value}`;
+  },
+  deleted: (record, currentDepth, fn, spaces, step) => {
+    const { key } = record;
+    const value = stringify(record.value, currentDepth, spaces, step);
+    const indent = currentDepth * step - 2;
+    return `${spaces.repeat(indent)}- ${key}: ${value}`;
+  },
+  equal: (record, currentDepth, fn, spaces, step) => {
+    const { key } = record;
+    const value = stringify(record.value, currentDepth, spaces, step);
+    const indent = currentDepth * step - 2;
+    return `${spaces.repeat(indent)}  ${key}: ${value}`;
+  },
+  changed: (record, currentDepth, fn, spaces, step) => {
+    const { key } = record;
+    const valueOld = stringify(record.valueOld, currentDepth, spaces, step);
+    const valueNew = stringify(record.valueNew, currentDepth, spaces, step);
+    const indent = currentDepth * step - 2;
+    return `${spaces.repeat(indent)}- ${key}: ${valueOld}\n${spaces.repeat(indent)}+ ${key}: ${valueNew}`;
+  },
+};
+
+const checkNode = arg => typesOfNode[arg];
 
 const render = (data) => {
-  const firstLevelSpaces = '  ';
-  //console.log(JSON.stringify(data));
+  const firstLevelSpaces = ' ';
+  const stepInSpacesBetweenLevels = 4;
+  // console.log(JSON.stringify(data));
   const iter = (record, currentDepth) => {
-    const { makeString } = checkNode(record.state);
-    return makeString(record, currentDepth, iter, firstLevelSpaces);
+    const makeString = checkNode(record.state);
+    return makeString(record, currentDepth, iter, firstLevelSpaces, stepInSpacesBetweenLevels);
   };
 
   const result = data.map(currentRecord => iter(currentRecord, 1)).join('\n');
